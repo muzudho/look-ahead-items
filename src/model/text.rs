5ffb8aefbada5382be480912e5b6c740a8b7c874
むずでょ <muzudho1@gmail.com>
2020-11-02 21:00:14 +0900

    iter

--- src/model/text.rs
+++ src/model/text.rs
@@ -39,11 +39,18 @@ impl Iterator for Text {
     //     * `Iterator`が終了した時は`None`を返し、
     //     * そうでなければ`Some`でラップされた値を返す。
     fn next(&mut self) -> Option<LookAheadCharacters> {
+        // 先読みは４つとします。
+        let num = 4;
+
         if self.look_ahead_characters.index < self.characters.len() {
-            let m = LookAheadCharacters::new(
-                self.look_ahead_characters.index,
-                &vec![self.characters[self.look_ahead_characters.index].clone()],
-            );
+            let mut vec = Vec::new();
+            for i in self.look_ahead_characters.index..self.look_ahead_characters.index + num {
+                if i < self.characters.len() {
+                    vec.push(self.characters[i].clone());
+                }
+            }
+
+            let m = LookAheadCharacters::new(self.look_ahead_characters.index, &vec);
             self.look_ahead_characters.index += 1;
             Some(m)
         } else {
